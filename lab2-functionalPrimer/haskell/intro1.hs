dfb4766683c89917c48629c045bdf8f75ab71086
GurberGink <gurbergink@gmail.com>
2026-01-27 22:39:51 +0000

    Add rules 14 and 15 to intro1.hs

--- lab2-functionalPrimer/haskell/intro1.hs
+++ lab2-functionalPrimer/haskell/intro1.hs
@@ -2,7 +2,7 @@ module Intro1 where
 
 -- define named constants:
 r1 = 1
-r2 = b^2 + 1/b where b = 2
+r2 = b^5 + 2/b where b = 4
 
 -- define a function:
 diff a b = abs (a - b)
@@ -24,6 +24,14 @@ r10 = zip [1..3] (map sqrt [1..3])
 r11 = zip [1..3] (map sqrt [1..2])
 
 r12 = print [1..3]
+
+r13 = a/2 + 2^b where a = 6; b = 10
+
+r14 = map reverse [["1","2","3","4","hello"]] 
+-- new rule added, map reverse works on list of list of strings
+
+r15 = map (+1.5) [1,2,3,4] 
+-- new rule added, (+x) works on list of numbers
 
 main :: IO ()
 main = pure ()
